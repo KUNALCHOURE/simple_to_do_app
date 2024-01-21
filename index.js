32f9300b279d998bc72243be8f0a1c0ebd5a5cf1
KUNAL CHOURE <chourekunal4@gmail.com>
2024-01-21 19:30:02 +0530

    updated

--- index.js
+++ index.js
@@ -4,8 +4,16 @@ let u=document.querySelector("#LIST");
 btn.addEventListener("click",function(){
   let item=document.createElement("li");
   item.innerText=inp.value;
+
+  let btn=document.createElement("button");
+  btn.innerText="Delete";
+  btn.classList.add("Delete");
+
+  item.appendChild(btn);
   u.appendChild(item);
+  
   inp.value="";
+
  
 
    
